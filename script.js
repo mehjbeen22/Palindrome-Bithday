47f57ee7ec9a6427bbaa8d85349d2fa5e8848759
Mehjbeen <mehjbeen22@navgurukul.org>
2023-08-26 11:44:29 +0530

    confetti event add

--- script.js
+++ script.js
@@ -150,6 +150,38 @@ const clickHandle = (e) => {
 
     if (isPalindrome) {
       showOutput.innerText = "🥳🤩Yay! Your BirthDaY is Palindrome 🥳🤩";
+
+      // Triggered a Confetti Event
+      const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+      };
+
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 40,
+          scalar: 1.2,
+          shapes: ["star"],
+        });
+
+        confetti({
+          ...defaults,
+          particleCount: 10,
+          scalar: 0.75,
+          shapes: ["circle"],
+        });
+      }
+
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+      // Event code End
     } else {
       let [ctr, nextDate] = getNextPalindromeDate(date);
 
